47c405f86acb22c2ef26b5739342d47b805d9833
wait-what <25844213+wait-what@users.noreply.github.com>
2021-11-08 23:16:37 +0200

    Add color, iteration input

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,37 @@
-const ITERATIONS: usize = 6;
-
-use image::{GenericImage, GenericImageView, DynamicImage, Rgba};
+use image::{DynamicImage, GenericImage, GenericImageView, Rgba};
 
 fn main() {
-    let mut image = DynamicImage::new_rgba8(3, 3);
+    let iterations: u8 = std::env::args().nth(1).expect("Provide amount of iterations as an argument (0-10 or so)").parse().unwrap();
+    if iterations > 8 {
+        println!("Warning: above 8 iterations - very resource intensive.");
+    };
 
-    for x in 0..3 {
-        for y in 0..3 {
-            image.put_pixel(x, y, Rgba(
-                if x == 1 && y == 1 { [0xff, 0xff, 0xff, 0xff] } else { [0x00, 0x00, 0x00, 0xff] }
-            ));
-        }
-    }
+    let mut image = DynamicImage::new_rgba8(1, 1);
+    image.put_pixel(0, 0, Rgba([0x2c, 0x07, 0x35, 0xff]));
 
-    for _ in 0..ITERATIONS {
+    for iteration in 0..iterations {
         let (width, height) = image.dimensions();
         let mut new_image = DynamicImage::new_rgba8(width * 3, height * 3);
 
         for x in 0..3 {
             for y in 0..3 {
-                if x == 1 && y == 1 {
-                    for part_x in 0..width {
-                        for part_y in 0..height {
-                            new_image.put_pixel(x * width + part_x, y * height + part_y, Rgba([0xff, 0xff, 0xff, 0xff]));
-                        }
-                    }
-                } else {
-                    for part_x in 0..width {
-                        for part_y in 0..height {
-                            new_image.put_pixel(x * width + part_x, y * height + part_y, image.get_pixel(part_x, part_y));
-                        }
+                for part_x in 0..width {
+                    for part_y in 0..height {
+                        new_image.put_pixel(
+                            x * width + part_x,
+                            y * height + part_y,
+                            if x == 1 && y == 1 {
+                                Rgba(match iteration % 4 {
+                                    0 => [0xF5, 0xB5, 0xFC, 0xFF],
+                                    1 => [0x96, 0xF7, 0xD2, 0xFF],
+                                    2 => [0xF0, 0xF6, 0x96, 0xFF],
+                                    3 => [0xFC, 0xB1, 0xB1, 0xFF],
+                                    _ => unreachable!()
+                                })
+                            } else {
+                                image.get_pixel(part_x, part_y)
+                            },
+                        );
                     }
                 }
             }
